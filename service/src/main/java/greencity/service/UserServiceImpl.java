2992bb7a90b7b4a15b77727682b754acb64e6ca2
Kirasin225 <mr.pom2005@gmail.com>
2025-04-16 22:05:49 +0300

    [65] add proper handling for sorting with invalid property

--- service/src/main/java/greencity/service/UserServiceImpl.java
+++ service/src/main/java/greencity/service/UserServiceImpl.java
@@ -31,6 +31,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -105,21 +106,25 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     public PageableAdvancedDto<UserManagementDto> findUserForManagementByPage(Pageable pageable) {
-        Page<User> users = userRepo.findAll(pageable);
-        List<UserManagementDto> userManagementDtos =
-            users.getContent().stream()
-                .map(user -> modelMapper.map(user, UserManagementDto.class))
-                .collect(Collectors.toList());
-        return new PageableAdvancedDto<>(
-            userManagementDtos,
-            users.getTotalElements(),
-            users.getPageable().getPageNumber(),
-            users.getTotalPages(),
-            users.getNumber(),
-            users.hasPrevious(),
-            users.hasNext(),
-            users.isFirst(),
-            users.isLast());
+        try {
+            Page<User> users = userRepo.findAll(pageable);
+            List<UserManagementDto> userManagementDtos =
+                    users.getContent().stream()
+                            .map(user -> modelMapper.map(user, UserManagementDto.class))
+                            .collect(Collectors.toList());
+            return new PageableAdvancedDto<>(
+                    userManagementDtos,
+                    users.getTotalElements(),
+                    users.getPageable().getPageNumber(),
+                    users.getTotalPages(),
+                    users.getNumber(),
+                    users.hasPrevious(),
+                    users.hasNext(),
+                    users.isFirst(),
+                    users.isLast());
+        } catch (PropertyReferenceException e) {
+            throw new BadRequestException(e.getMessage());
+        }
     }
 
     /**

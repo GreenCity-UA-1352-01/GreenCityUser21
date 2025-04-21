0d395292c5538eb2f8facccda6141f11ef484f16
dima <dimasowner7201@gmail.com>
2025-04-21 13:35:08 +0300

    added check for user email

--- service/src/main/java/greencity/service/EmailServiceImpl.java
+++ service/src/main/java/greencity/service/EmailServiceImpl.java
@@ -26,6 +26,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.context.Context;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -56,13 +57,13 @@ public class EmailServiceImpl implements EmailService {
      */
     @Autowired
     public EmailServiceImpl(JavaMailSender javaMailSender,
-        ITemplateEngine templateEngine,
-        UserRepo userRepo,
-        @Qualifier("sendEmailExecutor") Executor executor,
-        @Value("${client.address}") String clientLink,
-        @Value("${econews.address}") String ecoNewsLink,
-        @Value("${address}") String serverLink,
-        @Value("${sender.email.address}") String senderEmailAddress) {
+                            ITemplateEngine templateEngine,
+                            UserRepo userRepo,
+                            @Qualifier("sendEmailExecutor") Executor executor,
+                            @Value("${client.address}") String clientLink,
+                            @Value("${econews.address}") String ecoNewsLink,
+                            @Value("${address}") String serverLink,
+                            @Value("${sender.email.address}") String senderEmailAddress) {
         this.javaMailSender = javaMailSender;
         this.templateEngine = templateEngine;
         this.userRepo = userRepo;
@@ -75,7 +76,7 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendChangePlaceStatusEmail(String authorName, String placeName,
-        String placeStatus, String authorEmail) {
+                                           String placeStatus, String authorEmail) {
         log.info(LogMessage.IN_SEND_CHANGE_PLACE_STATUS_EMAIL, placeName);
         Map<String, Object> model = new HashMap<>();
         model.put(EmailConstants.CLIENT_LINK, clientLink);
@@ -89,8 +90,8 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendAddedNewPlacesReportEmail(List<PlaceAuthorDto> subscribers,
-        Map<CategoryDto, List<PlaceNotificationDto>> categoriesWithPlaces,
-        String notification) {
+                                              Map<CategoryDto, List<PlaceNotificationDto>> categoriesWithPlaces,
+                                              String notification) {
         log.info(LogMessage.IN_SEND_ADDED_NEW_PLACES_REPORT_EMAIL, null, null, notification);
         Map<String, Object> model = new HashMap<>();
         model.put(EmailConstants.CLIENT_LINK, clientLink);
@@ -106,15 +107,15 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendNewNewsForSubscriber(List<NewsSubscriberResponseDto> subscribers,
-        AddEcoNewsDtoResponse newsDto) {
+                                         AddEcoNewsDtoResponse newsDto) {
         Map<String, Object> model = new HashMap<>();
         model.put(EmailConstants.ECO_NEWS_LINK, ecoNewsLink);
         model.put(EmailConstants.NEWS_RESULT, newsDto);
         for (NewsSubscriberResponseDto dto : subscribers) {
             try {
                 model.put(EmailConstants.UNSUBSCRIBE_LINK, serverLink + "/newsSubscriber/unsubscribe?email="
-                    + URLEncoder.encode(dto.getEmail(), StandardCharsets.UTF_8.toString())
-                    + "&unsubscribeToken=" + dto.getUnsubscribeToken());
+                        + URLEncoder.encode(dto.getEmail(), StandardCharsets.UTF_8.toString())
+                        + "&unsubscribeToken=" + dto.getUnsubscribeToken());
             } catch (UnsupportedEncodingException e) {
                 log.error(e.getMessage());
             }
@@ -130,8 +131,8 @@ public class EmailServiceImpl implements EmailService {
         model.put(EmailConstants.NEWS_RESULT, newDto);
         try {
             model.put(EmailConstants.UNSUBSCRIBE_LINK, serverLink + "/newSubscriber/unsubscribe?email="
-                + URLEncoder.encode(newDto.getAuthor().getEmail(), StandardCharsets.UTF_8.toString())
-                + "&unsubscribeToken=" + newDto.getUnsubscribeToken());
+                    + URLEncoder.encode(newDto.getAuthor().getEmail(), StandardCharsets.UTF_8.toString())
+                    + "&unsubscribeToken=" + newDto.getUnsubscribeToken());
         } catch (UnsupportedEncodingException e) {
             log.error(e.getMessage());
         }
@@ -146,7 +147,7 @@ public class EmailServiceImpl implements EmailService {
      */
     @Override
     public void sendVerificationEmail(Long id, String name, String email, String token, String language,
-        boolean isUbs) {
+                                      boolean isUbs) {
         Map<String, Object> model = new HashMap<>();
         String baseLink = clientLink + "#/" + (isUbs ? "ubs" : "");
         model.put(EmailConstants.CLIENT_LINK, baseLink);
@@ -169,7 +170,7 @@ public class EmailServiceImpl implements EmailService {
         model.put(EmailConstants.CLIENT_LINK, clientLink);
         model.put(EmailConstants.USER_NAME, name);
         model.put(EmailConstants.APPROVE_REGISTRATION, clientLink + "#/auth/restore?" + "token=" + token
-            + PARAM_USER_ID + userId);
+                + PARAM_USER_ID + userId);
         String template = createEmailTemplate(model, EmailConstants.USER_APPROVAL_EMAIL_PAGE);
         sendEmail(email, EmailConstants.APPROVE_REGISTRATION_SUBJECT, template);
     }
@@ -184,13 +185,13 @@ public class EmailServiceImpl implements EmailService {
      */
     @Override
     public void sendRestoreEmail(Long userId, String userName, String userEmail, String token, String language,
-        boolean isUbs) {
+                                 boolean isUbs) {
         Map<String, Object> model = new HashMap<>();
         String baseLink = clientLink + "/#" + (isUbs ? "/ubs" : "");
         model.put(EmailConstants.CLIENT_LINK, baseLink);
         model.put(EmailConstants.USER_NAME, userName);
         model.put(EmailConstants.RESTORE_PASS, baseLink + "/auth/restore?" + "token=" + token
-            + PARAM_USER_ID + userId);
+                + PARAM_USER_ID + userId);
         changeLocale(language);
         model.put(EmailConstants.IS_UBS, isUbs);
         String template = createEmailTemplate(model, EmailConstants.RESTORE_EMAIL_PAGE);
@@ -272,6 +273,9 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendUserViolationEmail(UserViolationMailDto dto) {
+        userRepo.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new NotFoundException("User with email " + dto.getEmail() + " not found"));
+
         Map<String, Object> model = new HashMap<>();
         model.put(EmailConstants.CLIENT_LINK, clientLink);
         model.put(EmailConstants.USER_NAME, dto.getName());

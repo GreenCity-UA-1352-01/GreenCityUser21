26aa6a3245d6e013ebbd909e916fe854344e1e3d
Technivals <121890428+Technivals@users.noreply.github.com>
2025-04-21 21:07:14 +0300

    Update UserController.java

--- core/src/main/java/greencity/controller/UserController.java
+++ core/src/main/java/greencity/controller/UserController.java
@@ -394,10 +394,7 @@ public class UserController {
     })
     @GetMapping("/{userId}/profileStatistics/")
     public ResponseEntity<UserProfileStatisticsDto> getUserProfileStatistics(
-        @Parameter(description = "Id of current user. Cannot be empty.")  @PathVariable Long userId, @Parameter(hidden = true) @CurrentUserId Long currentUserId) {
-        if (userId != currentUserId) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
+        @Parameter(description = "Id of current user. Cannot be empty.") @PathVariable @CurrentUserId Long userId) {
         return ResponseEntity.status(HttpStatus.OK)
             .body(userService.getUserProfileStatistics(userId));
     }

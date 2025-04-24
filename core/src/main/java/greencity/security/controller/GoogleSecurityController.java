e7857636e6b16dff50cc69e9a8da3e731e783e23
SaenkoVova <vova080520@gmail.com>
2025-04-24 23:59:15 +0300

    [89] google token processing and user saving to db

--- core/src/main/java/greencity/security/controller/GoogleSecurityController.java
+++ core/src/main/java/greencity/security/controller/GoogleSecurityController.java
@@ -1,8 +1,16 @@
 package greencity.security.controller;
 
-import greencity.config.SecurityConfig;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import greencity.dto.user.UserVO;
+import greencity.security.dto.SuccessSignInDto;
+import greencity.security.dto.ownsecurity.OwnSignUpDto;
+import greencity.security.jwt.JwtTool;
+import greencity.security.service.OwnSecurityService;
+import greencity.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
@@ -14,12 +22,19 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/googleSecurity")
 @Validated
 @RequiredArgsConstructor
 public class GoogleSecurityController {
+
+    private final GoogleIdTokenVerifier googleIdTokenVerifier;
+    private final UserService userService;
+    private final JwtTool jwtTool;
+    private final OwnSecurityService ownSecurityService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping
     public ResponseEntity<?> googleGetCode() {
@@ -42,9 +57,10 @@ public class GoogleSecurityController {
                 .build();
     }
 
-    @GetMapping ("/callback")
-    public ResponseEntity<?>googleSignIn(@RequestParam String code) {
+    @GetMapping("/callback")
+    public ResponseEntity<?> googleSignIn(@RequestParam String code) {
         try {
+
             System.out.println("Success!" + code);
             RestTemplate restTemplate = new RestTemplate();
             String tokenEndpoint = "https://oauth2.googleapis.com/token";
@@ -62,7 +78,42 @@ public class GoogleSecurityController {
             HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
             ResponseEntity<Map> tokenResponse = restTemplate.postForEntity(tokenEndpoint, request, Map.class);
 
-            return ResponseEntity.ok(tokenResponse);
+            GoogleIdToken googleIdToken = googleIdTokenVerifier.verify((String) tokenResponse.getBody().get("id_token"));
+
+            if (googleIdToken != null) {
+                GoogleIdToken.Payload payload = googleIdToken.getPayload();
+                System.out.println(payload.toString());
+                String email = payload.getEmail();
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+
+                UserVO user = userService.findByEmail(email);
+
+
+                if (user == null) {
+                    var ownSignInDto = OwnSignUpDto
+                            .builder()
+                            .name(name)
+                            .email(email)
+                            .isUbs(false)
+                            .password(passwordEncoder.encode(UUID.randomUUID().toString()))
+                            .build();
+                    ownSecurityService.signUp(ownSignInDto, "ua");
+                }
+
+
+                UserVO savedUser = userService.findByEmail(email);
+
+                String accessToken = jwtTool.createAccessToken(savedUser.getEmail(), savedUser.getRole());
+                String refreshToken = jwtTool.createRefreshToken(savedUser);
+
+                return ResponseEntity.ok(new SuccessSignInDto(savedUser.getId(), accessToken, refreshToken, savedUser.getName(), false));
+            } else {
+                System.out.println("Invalid ID token");
+            }
+
+
+            return ResponseEntity.ok().build();
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
